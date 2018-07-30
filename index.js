efe8d41a376142c58241858e033df983f3bb778b
luanpascoal14 <41136805+luanpascoal14@users.noreply.github.com>
2018-07-30 13:02:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,6 @@ bot.on('guildMemberRemove', member => {
         let canalSAI = member.guild.channels.get('452259852547522570');
         canalSAI.send('👈 ' + member.user.username + ' você sempre será Bem Vindo ✔');
     }
-    if(member.guild.id === '437625052775710753') {
-        let canalBV = member.guild.channels.get('437662840980242432');
-        canalBV.send('Bem-Vindo, ' + member.user.username + ' ao grupo ŘΔƤØŞΔĆŘΔ₣Ŧ, chame seus amigos para se divertir!');
-        member.send('Obrigado por entrar no **' + member.guild.name + '** ' + member.user.username + '! Chame seus amigos para sé divertir com você! https://discord.gg/26MPNnh');
-    }
 });
 
 
